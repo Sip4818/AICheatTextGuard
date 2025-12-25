0071ba47a90897833ea97cd580460214d179b0d2
Sip4818 <sip4818@gmail.com>
2025-12-25 19:34:16 +0530

    port fix in dockerfile.monolithic

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Start the Backend file (FastAPI)
 # We use 'backend.app' because it's inside the backend folder
-uvicorn app:app --host 0.0.0.0 --port 8000 &
+uvicorn app:app --host 0.0.0.0 --port 8080 &
 
 # Start the UI file (Streamlit)
 streamlit run ui/streamlit_app.py --server.port 7860 --server.address 0.0.0.0
