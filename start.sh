23d61029774246cabca1db2dc4650f963c0239bc
Sip4818 <sip4818@gmail.com>
2025-12-29 12:37:28 +0530

    improve model

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@
 # We use 'backend.app' because it's inside the backend folder
 uvicorn app:app --host 0.0.0.0 --port 8080 &
 
+
 # Start the UI file (Streamlit)
 streamlit run ui/streamlit_app.py --server.port 7860 --server.address 0.0.0.0
